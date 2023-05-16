db08dc8bd9403ebe4cc31ea6015f57466ad940f2
Adrian Florczak <kontakt@adrianflorczak.pl>
2023-05-16 22:29:40 +0200

    first HelloWorld RestAPI

---
+++ server.js
@@ -0,0 +1,13 @@
+import app from "./app.js";
+
+app.set('PORT', process.env.PORT || 8080);
+app.set('NODE_ENV', process.env.NODE_ENV || 'PRODUCTION');
+
+app.listen(app.get('PORT'), () => {
+    console.log(`
+        Server running
+        --------------
+        PORT: ${app.get('PORT')}
+        NODE_ENV: ${app.get('NODE_ENV')}
+    `)
+})
